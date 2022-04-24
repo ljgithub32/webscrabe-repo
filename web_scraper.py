135f6ff86051368d0f0d2feb869cb3a18ee9aae0
Luke <ukluke123@hotmail.co.uk>
2022-04-24 23:30:24 +0100

    added comments for reference

--- web_scraper.py
+++ web_scraper.py
@@ -2,12 +2,19 @@ from bs4 import BeautifulSoup
 import requests
 
 html_text = requests.get("https://www.box.co.uk/rtx-3080-graphics-cards").text  # pulls html from webpage
-# print(html_text)
-soup = BeautifulSoup(html_text, "lxml")
 
-for gpus in soup.find_all("div", class_="product-list-item"):
+soup = BeautifulSoup(html_text, "lxml")  # creates soup object which is the html page
 
-    item_name = gpus.find("div", class_="p-list-title-wrapper").h3.text
-    item_price = gpus.find("span", class_="pq-price").text
-    item_link = gpus.find("div", class_="p-list-title-wrapper").h3.a["href"]
-    print(f"GPU name: {item_name}, Price: {item_price}, Link: {item_link}")
+for gpus in soup.find_all("div", class_="product-list-item"):  # deepest layer which still contains all info we want
+
+    # looping as we want to go through each "product-list-item" on that page,
+
+    item_name = gpus.find("div", class_="p-list-title-wrapper").h3.text  # parsing out title that is within h3 tag
+
+    item_price = gpus.find("span", class_="pq-price").text  # parsing out price which is within a span with a class
+
+    item_link = gpus.find("div", class_="p-list-title-wrapper").h3.a["href"] # parsing out the link, [] picks out link
+
+    print(f"GPU name: {item_name}, Price: {item_price}, Link: {item_link}") # formatting our output
+
+    print()
