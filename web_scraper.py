2868c360f91498115a83b21ec802c8f6589d25fe
Luke <ukluke123@hotmail.co.uk>
2022-04-24 23:06:17 +0100

    simplified code for scraping rtx 3080 prices from box

--- web_scraper.py
+++ web_scraper.py
@@ -7,8 +7,6 @@ soup = BeautifulSoup(html_text, "lxml")
 
 for gpus in soup.find_all("div", class_="product-list-item"):
 
-    item = gpus.find("div", class_="p-list-title-wrapper")
-    item_name = item.h3.a.text
-    prices = gpus.find("div", class_="p-list-price-wrapper")
-    item_price = prices.span.text
+    item_name = gpus.find("div", class_="p-list-title-wrapper").h3.text
+    item_price = gpus.find("span", class_="pq-price").text
     print(f"GPU name: {item_name}, Price: {item_price}")
